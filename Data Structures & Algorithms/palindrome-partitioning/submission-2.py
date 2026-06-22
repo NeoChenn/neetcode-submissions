5d44f87326c6439cee257e21ccfa6b5b425275d2
Neo Chen Jiang <152399138+NeoChenn@users.noreply.github.com>
2026-06-23 00:14:55 +0100

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def partition(self, string: str) -> List[List[str]]:
+        def checkPalindrome(s):
+            i, j = 0, len(s) - 1
+            while i <= j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+
+        res = []
+        attempt = []
+        def backtrack(remaining):
+            if not remaining:
+                for substring in attempt:
+                    if not checkPalindrome(substring):
+                        return
+                res.append(attempt.copy())
+                return
+        
+            for i in range(len(remaining)):
+                attempt.append(remaining[:(i + 1)])
+                backtrack(remaining[i + 1: len(remaining)])
+                attempt.pop()
+
+        backtrack(string)
+        return res
+
+            
