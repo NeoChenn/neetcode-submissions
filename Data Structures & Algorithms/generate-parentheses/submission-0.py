e699e30768605940bc1e44f90fe9c8bdd75f12bf
Neo Chen Jiang <152399138+NeoChenn@users.noreply.github.com>
2026-06-22 17:26:10 +0100

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:     
+    def generateParenthesis(self, n: int) -> List[str]:
+        #base case: length of string == 2n. Append and return if stack empty. Otherwise, just return
+        #choices: "(" and append to stack. or ")" and pop from stack. 
+                # Can choose ")" only if stack not empty. 
+                # return if either leftps or rightps > n
+        res = []
+        string = []
+        stack = []
+
+        def backtrack(leftps, rightps):
+            if len(string) == 2*n:
+                if not stack:
+                    res.append("".join(string.copy()))
+                    return
+                return
+            if leftps > n or rightps > n:
+                return
+            
+            string.append("(")
+            stack.append("(")
+            backtrack(leftps + 1, rightps)
+            string.pop()
+            stack.pop()
+
+            if stack:
+                string.append(")")
+                stack.pop()
+                backtrack(leftps, rightps + 1)
+                string.pop()
+                stack.append("(")
+
+        backtrack(0, 0)
+        return res
+
+
+
+        
