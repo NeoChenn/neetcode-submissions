56aa3e28f8cfa6aa6c29ff90c7ab8b9a5983a66e
Neo Chen Jiang <152399138+NeoChenn@users.noreply.github.com>
2026-06-25 23:51:00 +0100

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        #no loops and fully connected.
+        #check for loops ONCE starting from one node. 
+        #if any node not in visited set or loop is found, return false
+
+        adjList = {}
+        for i in range(n):
+            adjList[i] = []
+        for node1, node2 in edges:
+            adjList[node1].append(node2)
+            adjList[node2].append(node1)
+
+        visited, path = set(), set()
+        
+        def dfs(curr, parent):
+            if curr in path:
+                return False
+            if curr in visited:
+                return True
+
+            path.add(curr)
+            for neighbor in adjList[curr]:
+                if neighbor != parent:
+                    if not dfs(neighbor, curr):
+                        return False
+            path.remove(curr)
+            visited.add(curr)
+            return True
+            
+
+        if not dfs(0, None):
+            return False
+        for i in range(n):
+            if i not in visited:
+                return False
+        return True
