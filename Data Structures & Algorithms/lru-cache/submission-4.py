85aad07b63a9add6351aa135c5cacea3cba1e516
Neo Chen Jiang <152399138+NeoChenn@users.noreply.github.com>
2026-07-13 15:12:28 +0200

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,42 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        #hashmap to store key-value pairs
+        #minheap to determine least used key. 
+        #sort based on a time variable that increases for each get/put operation
+
+        self.pairs = {} #key : [value, time]
+        self.heap = []
+        self.time = 0
+
+    def get(self, key: int) -> int:
+        #returns value if exists, otherwise returns -1
+        if key not in self.pairs:
+            return -1
+        self.time += 1
+        self.pairs[key][1] = self.time
+        self.heap = []
+        for item in list(self.pairs.items()):
+            heapq.heappush(self.heap, (item[1][1], item[0]))
+        return self.pairs[key][0]
+
+    def put(self, key: int, value: int) -> None:
+        #update value if key exists
+        #if not, add key-value pair to cache
+        #if introducing new pair causes cache to exceed its capacity, remove the least recently used key
+        self.time += 1
+        if key in self.pairs:
+            self.pairs[key][0] = value
+            self.pairs[key][1] = self.time
+            self.heap = []
+            for item in list(self.pairs.items()):
+                heapq.heappush(self.heap, (item[1][1], item[0]))
+        elif key not in self.pairs and len(self.pairs) + 1 <= self.capacity:
+            self.pairs[key] = [value, self.time]
+            heapq.heappush(self.heap, (self.time, key))
+        else:
+            lru = heapq.heappop(self.heap)
+            self.pairs.pop(lru[1])
+            self.pairs[key] = [value, self.time]
+            heapq.heappush(self.heap, (self.time, key))
