a5a720b240cc5e4917d00a800f6b9b82ca8bba79
Neo Chen Jiang <152399138+NeoChenn@users.noreply.github.com>
2026-07-14 17:00:12 +0200

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        q = deque()
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 0:
+                    q.append((r, c))
+
+        level = 1
+        while q:
+            for i in range(len(q)):
+                currRow, currCol = q.popleft()
+                
+                for r, c in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    newRow, newCol = currRow + r, currCol + c
+                    if 0 <= newRow < len(grid) and 0 <= newCol < len(grid[0]) and grid[newRow][newCol] == 2147483647:
+                        grid[newRow][newCol] = level  # mark immediately when enqueuing
+                        q.append((newRow, newCol))
+            level += 1
