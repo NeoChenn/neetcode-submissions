6d55171cf155540d964e3f9a8f9328596b1c09cc
Neo Chen Jiang <152399138+NeoChenn@users.noreply.github.com>
2026-07-20 00:07:39 +0200

    Add: find-k-closest-elements - submission-3

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        #[1, 3, 5, 7, 9, 11, 13] #12
+        #find largest value smaller or equal to x. Two pointer with that and the next value
+        #edge cases. If x <= arr[0] or x >= arr[-1]
+
+        l, r = 0, len(arr) - 1
+        z = 0
+        while l <= r:
+            mid = (l + r) // 2
+            if arr[mid] > x:
+                r = mid - 1
+            elif arr[mid] <= x:
+                z = max(z, mid)
+                l = mid + 1
+
+        l, r = z, z + 1
+        while r - l - 1 != k:
+            if r >= len(arr):
+                l -= 1
+            elif l < 0:
+                r += 1
+            elif abs(arr[l] - x) <= abs(arr[r] - x):
+                l -= 1
+            else:
+                r += 1
+
+        return arr[l + 1:r]
