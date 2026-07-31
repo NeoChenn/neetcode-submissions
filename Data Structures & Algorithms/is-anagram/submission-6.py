42f0d4c224ed35f87cb33b902d9df56c97dd362a
Neo Chen Jiang <152399138+NeoChenn@users.noreply.github.com>
2026-07-31 12:03:32 +0200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sMap = {}
+        tMap = {}
+        for c in s:
+            if c not in sMap:
+                sMap[c] = 0
+            sMap[c] += 1
+        for c in t:
+            if c not in tMap:
+                tMap[c] = 0
+            tMap[c] += 1
+
+        return sMap == tMap
